895e89f50f95881319eb726b351ce29e0e236442
Vathsal Veena Shashidhar <picasiovs@gmail.com>
2019-04-14 17:04:41 -0700

    Adding a main program

---
+++ DataStructures/main_program.py
@@ -0,0 +1,19 @@
+"""
+MAIN PROGRAM 
+"""
+
+from Stack import Stack
+
+s=Stack()
+
+print "Is list empty ? :" , s.isEmpty()
+s.push(4)
+s.push('dog')
+print(s.peek())
+s.push(True)
+print(s.size())
+print(s.isEmpty())
+s.push(8.4)
+print(s.pop())
+print(s.pop())
+print(s.size())
